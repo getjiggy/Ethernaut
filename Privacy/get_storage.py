119a3a179cdba91ac9ac804df2d0b6dedb15632e
getjiggy <74795898+getjiggy@users.noreply.github.com>
2021-11-30 13:57:48 -0500

    Update get_storage.py

--- Privacy/get_storage.py
+++ Privacy/get_storage.py
@@ -1,10 +1,5 @@
-import json
-import time
-from itertools import permutations, combinations
-from pprint import pprint
-import asyncio
 from web3 import Web3, HTTPProvider
-import os
+
 
 provider = Web3.HTTPProvider('https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161')
 w3 = Web3(provider)
